f183fee5ecad5846d59e08de05a8a980091e7853
Ochowicz <114605042+Ochowicz@users.noreply.github.com>
2023-04-04 22:56:41 +0200

    Add files via upload

---
+++ chapter 4 - Magic_8_Ball.bat
@@ -0,0 +1,2 @@
+@py C:\MyPythonScripts\chapter_4_-_Magic_8_Ball.py %*
+@pause
---
+++ chapter_4_-_Magic_8_Ball.py
@@ -0,0 +1,12 @@
+#! python3
+import random
+massages = ['It is certain',
+    'It is decidedly so',
+    'Yes definitely',
+    'Reply hazy try again',
+    'Ask again later',
+    'Concentrate and ask again',
+    'My reply is no',
+    'Outlook not so good',
+    'Very doubtful']
+print(massages[random.randint(0, len(massages) - 1)])
